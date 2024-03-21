d23ab352b86f95da26c06989fd47d7daed43e528
Артем <tema.petrosyan@gmail.com>
2024-03-21 12:08:57 +0300

    Code cleaning

--- src/main/java/com/example/List.java
+++ src/main/java/com/example/List.java
@@ -1,63 +1,57 @@
 package com.example;
 
-import java.io.Console;
-
 /**
- * List односвязный список
- * @param head указатель на первый элемент
- * @param size размер списка
+ * @author Артем
+ * <p>
+ * Односвязный список
  */
 public class List {
 	private Node head;
 	private int size;
 
-	//конструктор по умолчанию
+	/**
+	 * Конструктор по умолчанию
+	 */
 	public List() {
 		this.head = null;
 		this.size = 0;
 	}
 
 	/**
-	 * добавление элемента
-	 * @param num число, которое нужно вставить
-	 * @param ind позиция, куда нужно поставить
+	 * Добавление элемента
+	 * @param num элемент для вставки
+	 * @param ind позиция для вставки
 	 */
-	public void addElem(int num, int ind){
-		if(size == 0){
+	public void addElem(int num, int ind) {
+		if(size == 0)
 			head = new Node(num, null);
-		}
 		else if(ind == 0)
-		{
 			head = new Node(num, head);
-		}
-		else{
+		else {
 			Node ptr = head;
-			for(int i = 0; i < Math.min(size, ind)-1; i++){ // если с индексом перестарались, добавляется в самый конец
+			// если с индексом перестарались, добавляется в самый конец
+			for(int i = 0; i < Math.min(size, ind) - 1; i++)
 				ptr = ptr.getNextElem();
-			}
 			ptr.setNextElem(new Node(num, ptr.getNextElem()));
 		}
 		size++;
 	}
 
 	/**
-	 * удаление элемента
+	 * Удаление элемента
 	 * @param ind индекс удаляемого элемента
 	 * @return флаг о выполнении операции
 	 */
-	public boolean delElem(int ind){
-		if(ind>=size){
+	public boolean delElem(int ind) {
+		if(ind >= size)
 			return false;
-		}
-		else{
-			if(ind == 0){
+		else {
+			if(ind == 0)
 				head = head.getNextElem();
-			}
-			else{
+			else {
 				Node ptr = head;
-				for(int i = 0; i < ind-1; i++){
+				for(int i = 0; i < ind-1; i++)
 					ptr = ptr.getNextElem();
-				}
 				ptr.setNextElem(ptr.getNextElem().getNextElem());
 			}
 			size--;
@@ -66,24 +60,21 @@ public class List {
 	}
 
 	/**
-	 * редактирование элемента
-	 * @param num число на которое меняется элемент
-	 * @param ind индес, где заменить надо
+	 * Редактирование элемента
+	 * @param num число, на которое меняется элемент
+	 * @param ind индес элемента для замены
 	 * @return флаг о выполнении операции
 	 */
-	public boolean editElem(int num, int ind){
-		if(ind>=size){
+	public boolean editElem(int num, int ind) {
+		if(ind >= size)
 			return false;
-		}
-		else{
-			if(ind == 0){
+		else {
+			if(ind == 0)
 				head = head.getNextElem();
-			}
-			else{
+			else {
 				Node ptr = head;
-				for(int i = 0; i < ind; i++){
+				for(int i = 0; i < ind; i++)
 					ptr = ptr.getNextElem();
-				}
 				ptr.setNum(num);
 			}
 			return true;
@@ -93,17 +84,16 @@ public class List {
 	/**
 	 * Получение значения элемента
 	 * @param ind индекс
-	 * @return число в указанной ячейке
+	 * @return значение элемента с данным индексом
+	 * @throws ArrayIndexOutOfBoundsException попытка получить элемент с несуществующим индексом
 	 */
-	public int getElem(int ind){
-		if(ind >= size){
-			return Integer.MIN_VALUE; // я не придумал, как умнее намекнуть пользователю, что он выбрал не тот индекс, хотел null, но как я понял, в java на инт null нет
-		}	
-		else{
+	public int getElem(int ind) {
+		if(ind >= size)
+			throw new ArrayIndexOutOfBoundsException("No such index in list");
+		else {
 			Node ptr = head;
-			for(int i = 0; i < ind; i++){
+			for(int i = 0; i < ind; i++)
 				ptr = ptr.getNextElem();
-			}
 			return ptr.getNum();
 		}
 	}
@@ -111,11 +101,11 @@ public class List {
 	@Override
 	public String toString() {
 		Node ptr = head;
-		String res = "";
+		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < size; i++){
-			res += ptr.getNum() + " ";
+			sb.append(ptr.getNum()).append(" ");
 			ptr = ptr.getNextElem();
 		}
-		return res;
+		return sb.toString();
 	}
 }
